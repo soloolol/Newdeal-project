23d9c295fff08241380839fa7b4a97bb2e3091c7
sol <soloolol@naver.com>
2022-09-14 02:18:54 +0900

    Update LoginNaver.vue

--- front1/src/components/LoginNaver.vue
+++ front1/src/components/LoginNaver.vue
@@ -21,7 +21,7 @@ const callbackNaver = async ()=>{
         console.log("네이버로그인중3",window.naverLogin)
 
         let data = '';
-        await naverLogin.getLoginStatus(function(status) {
+        await naverLogin.getLoginStatus( async function(status) {
             if (status) {
                 const info = {
                 id: naverLogin.user.id,
@@ -30,7 +30,7 @@ const callbackNaver = async ()=>{
                 nickname: naverLogin.user.nickname,
                 profile_image: naverLogin.user.profile_image,
                 }
-                data = info
+                data = await info
                 console.log(data)
             } else {
                 console.log('AccessToken이 올바르지 않습니다.') 
