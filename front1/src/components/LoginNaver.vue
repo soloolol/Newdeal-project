2384de748cc8546b80172ad47a3267bb894d86b2
sol <soloolol@naver.com>
2022-09-14 01:45:35 +0900

    Update LoginNaver.vue

--- front1/src/components/LoginNaver.vue
+++ front1/src/components/LoginNaver.vue
@@ -16,9 +16,9 @@ export default {
         }
     },
 
-    async created(){
+    async mounted(){
         // 네이버 사용자 프로필 조회
-	    const data = await callbackNaver()
+	    const data = await this.callbackNaver()
         console.log("네이버로그인중",data)
         
         await this.$store.dispatch("userGetAction", {data, social});
@@ -28,14 +28,16 @@ export default {
 
     methods:{
         callbackNaver(){
+            console.log("네이버로그인중1",window.naverLogin)
+
             const naverLogin = new naver.LoginWithNaverId({
             clientId: '2jr941k4E1YtJ1JN3Cw7'
             })
-            console.log("네이버로그인중",window.naverLogin)
+            console.log("네이버로그인중2",window.naverLogin)
 
             naverLogin.init()
 
-            console.log("네이버로그인중 init 다음",window.naverLogin)
+            console.log("네이버로그인중3",window.naverLogin)
 
             naverLogin.getLoginStatus(function(status) {
             if (status) {
