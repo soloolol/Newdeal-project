f55a4c073809c2f9418c9a2c495f138b98773e37
sol <soloolol@naver.com>
2022-09-16 13:20:32 +0900

    test

--- front1/src/store/index.js
+++ front1/src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import createPersistedState from 'vuex-persistedstate';
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
     userInfo: undefined,
     fishTmp: undefined,
@@ -68,5 +71,10 @@ export default new Vuex.Store({
   },
 
   modules: {
-  }
+  },
+  plugins:[
+    createPersistedState({
+      path:[this]
+    })
+  ],
 })
