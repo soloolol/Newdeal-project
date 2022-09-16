97f15694a0ba774a1cf72e5a5973b46bb2a31499
sol <soloolol@naver.com>
2022-09-16 15:15:32 +0900

    Update Result.vue

--- front1/src/components/Result.vue
+++ front1/src/components/Result.vue
@@ -11,40 +11,31 @@
         ></v-img>
 
         <v-card-title>
-        어종 : {{spc}}
+        {{spc}}
         </v-card-title>
 
         <v-card-title>
-        길이 : {{cm}}
+        길이 : {{cm}} cm
         </v-card-title>
 
         <v-card-actions>
-        <v-btn
-            color="orange lighten-2"
-            text
-        >
-            어종 정보
-        </v-btn>
+            <v-btn
+                color="orange lighten-2"
+                text
+            >
+                어종 정보
+            </v-btn>
 
-        <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-        <v-btn
-            icon
-            @click="show = !show"
-        >
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-        </v-btn>
+            <v-btn
+                icon
+                @click="show = !show"
+            >
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
         </v-card-actions>
 
-        <v-expand-transition>
-        <div v-show="show">
-            <v-divider></v-divider>
-
-            <v-card-text>
-            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-            </v-card-text>
-        </div>
-        </v-expand-transition>
     </v-card>
   </v-container>
 </template>
