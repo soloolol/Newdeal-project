5c294668e56922aa65a335e97b22bd05ac4a3ff6
sol <soloolol@naver.com>
2022-09-16 11:16:31 +0900

    Update Login.vue

--- front1/src/components/Login.vue
+++ front1/src/components/Login.vue
@@ -27,16 +27,27 @@
       max-width="400"
       align-items="center"
     >
-      <v-avatar size="110" color="red" v-if="this.$store.state.userInfo.thumbnail===null">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-      <v-avatar size="110" color="orange" v-else>
-        <img
-          :src="this.$store.state.userInfo.thumbnail"
-          alt="userProfile"
-        >
-      </v-avatar>
-      <v-title justify="center" text-align="center" align-items="center">{{this.$store.state.userInfo.nickname}}</v-title>
+      <v-row justify="center" >
+        <v-col cols="12">
+          <v-avatar size="110" color="red" v-if="this.$store.state.userInfo.thumbnail===null">
+            <v-icon dark>mdi-account-circle</v-icon>
+          </v-avatar>
+
+          <v-avatar size="110" color="orange" v-else>
+            <img
+              :src="this.$store.state.userInfo.thumbnail"
+              alt="userProfile"
+            >
+          </v-avatar>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <v-title justify="center" text-align="center" align-items="center">{{this.$store.state.userInfo.nickname}}</v-title>
+        </v-col>
+      </v-row>
+
       <v-row justify="center" >
         <v-col cols="12">
           <v-row justify="center" class="mb-10">
